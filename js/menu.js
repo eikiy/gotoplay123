4b56d0f76e3cfd565dbeb64333a9ac5dcb30d5d9
eikiy <eikiybaby@gmail.com>
2018-03-30 23:51:28 +0800

    修改：首頁 新增：登入、註冊、忘記密碼/重設密碼、重設密碼 新增靜態頁：關於GoToPlay123、關於會員約定事項、網路政策、活動風險告知及免責聲明

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,23 @@
+//點右邊top
+
+$(function(){
+　$(window).load(function(){
+　　$(window).bind('scroll resize', function(){
+　　var $this = $(this);
+　　var $this_Top=$this.scrollTop();
+
+　　//當高度小於100時，關閉區塊 
+　　if($this_Top < 500){
+  $('a.go_top').stop(true, true).fadeOut();
+  // $('.top-bar').stop(true, true).fadeOut(0);
+　　　}
+　　　　if($this_Top > 500){
+　　　　$('a.go_top').stop(true, true).fadeIn();
+　　　 }$('a.go_top').click(function(){ $('html,body').stop(true,false).animate({scrollTop:0}, 800); }); 
+　　}).scroll();
+
+　});
+});
 
  $(function(){
   $("ul.userBox li p.login").click(function(){
@@ -6,15 +26,6 @@
     });
  });
 
- // $(function(){
- //  $(".HeaderBox .Menu ul.menu li").hover(function(){
- //        $(this).find("p.color").addClass('btn_in');
- //        $(this).find(".subMenuBox").stop(true, true).fadeToggle(0);
- //        }, function(){
- //          $(this).find("p.color").removeClass('btn_in');
- //        }
- //    });
- // });
 $(function(){
   $('.HeaderBox .Menu ul.menu li').hover(function(){
       $(this).find('p.color').addClass('btn_in');
@@ -25,16 +36,16 @@ $(function(){
   });
 });
 
- $(function(){
-  var body = $("body");
-  var width = body.width()
+ // $(function(){
+ //  var body = $("body");
+ //  var width = body.width()
 
-  if(width < 1000)
-  {
-      $(".sideMenu").mouseleave(function(){
-        $(this).find("ul.mainMenu li ul.subMenu").stop(true, true).hide();
-    return false;
-    });
-  }
-  return false;
- });
+ //  if(width < 1000)
+ //  {
+ //      $(".sideMenu").mouseleave(function(){
+ //        $(this).find("ul.mainMenu li ul.subMenu").stop(true, true).hide();
+ //    return false;
+ //    });
+ //  }
+ //  return false;
+ // });
